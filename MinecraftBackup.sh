8a5588964eeccbbf588f5e33376500ded93eb286
HaydenElza <hayden.elza@gmail.com>
2015-06-01 21:56:14 -0500

    Added backup script.

---
+++ MinecraftBackup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIRECTORY=/home/$(whoami)/.minecraftbackups/
+
+if [ ! -d "$DIRECTORY" ]; then
+	echo mkdir $DIRECTORY
+fi
+
+OF=/home/$(whoami)/.minecraftbackups/$(date +%F).tar.gz
+
+echo "Backing up the .minecraft folder to $OF" 
+
+tar -czvf $OF -C /home/$(whoami)/.minecraft/ .
+
+if [ -a $OF ]; then
+	echo "Backup successful!"
+else
+	echo "Something went wrong."
+fi
